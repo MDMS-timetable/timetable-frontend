78ae0026c6d9c110f56b2ac192bb2e61e28eb466
anys34 <anyeseong34@gmail.com>
2023-05-16 09:14:57 +0900

    기본 요소 추가

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,19 @@ import React, { useEffect, useState } from "react";
 import axios from 'axios';
 
 function App() {
-  
+
+  const [timetalbeData, setTimetableData] = useState(null)
+
+  const fetchData = async() => {
+    setTimetableData(null);
+    const response = await axios.get('http://localhost:2500/timetable')
+    setTimetableData(response.data);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
 }
 
 export default App
--- server/server.js
+++ server/server.js
@@ -11,10 +11,50 @@ app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', function(req, res) {
-  res.send('main');
+const schoolFinder = (schoolName, region) => (schoolList) => {
+const targetSchool = schoolList.find((school) => {
+    return school.region === region && school.name.includes(schoolName);
+});
+return targetSchool;
+};
+
+timetable
+.init({ cache: 1000 * 60 * 60 }) // 캐시 1시간동안 보관
+.then(() => timetable.search('만덕중학교'))
+.then(schoolFinder('만덕중학교', '만덕'))
+.then((school) => timetable.setSchool(59955))
+.then(() => {
+    // 수업시간정보
+    Promise.all([timetable.getTimetable()]).then((result) => {
+    console.log(result);
+    
+    // result[학년][반][요일][교시]
+    for (var i = 0; i < 8; i++) {
+        if (result[0][1][1][0][i].subject !== '')
+        console.log(result[0][1][1][0][i]);
+    }
+
+    app.get('/timetable', function(req, res) {
+        // res.send(JSON.stringify(result[0][1][1][0]));
+        // JSON.stringify(result, null, 2);
+        // for (var i = 0; i < 8; i++) {
+        // if (result[0][1][1][0][i].subject !== '')
+        //     res.send(result[0][1][1][0][i]);
+        // }
+        res.json(result);
+    });
+    });
+
+    // 시간표
+    Promise.all([timetable.getClassTime()]).then((result) => {
+    app.get('/schedule', function(req, res) {
+        res.send(result);
+    });
+    });
+
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
