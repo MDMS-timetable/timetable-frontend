77ac578c6b901faa9a24699475e730cc743392ec
seonghoon07 <kkukkuli07@gmail.com>
2023-05-24 09:26:06 +0900

    ADD :: MondayClick

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,6 @@ const App = () => {
   const [timeTableData, setTimeTableData] = useState();
   const [selectedGrade, setSelectedGrade] = useState('');
   const [selectedClass, setSelectedClass] = useState('');
-  const [cancelToken, setCancelToken] = useState(null); // 요청 취소 토큰
   const [currentDateTime, setCurrentDateTime] = useState('');
 
   // 시간표
@@ -35,38 +34,6 @@ const App = () => {
     }
   }, []);
 
-  useEffect(() => {
-    // 선택 값이 변경될 때마다 로컬 스토리지에 저장
-    localStorage.setItem('selectedGrade', selectedGrade);
-    localStorage.setItem('selectedClass', selectedClass);
-
-    // 이전 요청 취소
-    if (cancelToken) {
-      cancelToken.cancel('Previous request canceled');
-    }
-
-    // 요청 취소 토큰 생성
-    const source = axios.CancelToken.source();
-    setCancelToken(source);
-
-    // 선택 값이 존재할 경우에만 요청 보냄
-    if (selectedGrade && selectedClass) {
-      axios
-        .post('http://localhost:8080/viewtimetable', { grade: selectedGrade, class: selectedClass }, { cancelToken: source.token })
-        .then(response => {
-          setTimeTableData(response.data);
-        })
-        .catch(error => {
-          if (axios.isCancel(error)) {
-            console.log('Request canceled', error.message);
-          } else {
-            console.log(error);
-          }
-        });
-    }
-  }, [selectedGrade, selectedClass]);
-
-
   return (
     <div>
       <Select 
@@ -76,6 +43,7 @@ const App = () => {
       setSelectedGrade = {setSelectedGrade}
       currentDateTime = {currentDateTime}
       setCurrentDateTime = {setCurrentDateTime}
+      setTimeTableData = {setTimeTableData}
       />
       <div>
         <RenderDayData 
