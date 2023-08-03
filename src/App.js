ce0603e01e06c3999308d08e756d1ae2b0fb6129
jyh071116 <jyh071116@gmail.com>
2023-08-04 06:16:07 +0900

    EDIT :: lunch페이지명 수정

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import './Style.scss';
 
 //Components
 import TimeTable from './Pages/TimeTable';
-import Launch from './Pages/Launch';
+import Lunch from './Pages/Lunch';
 
 const App = () => {
     return (
         <Routes>
             <Route path='/' Component={TimeTable} />
-            <Route path='/launch' Component={Launch} />
+            <Route path='/lunch' Component={Lunch} />
         </Routes>
     );
 };
