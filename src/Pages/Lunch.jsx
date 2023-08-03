787e19d0dfa9d3b0d953e6d6f2be7cfb555514ad
Jamkris <dltmgus1412@gmil.com>
2023-08-04 08:22:18 +0900

    Del

--- src/Pages/Lunch.jsx
+++ src/Pages/Lunch.jsx
@@ -1,33 +1,38 @@
-import { useState } from "react";
-import React, { useEffect } from "react";
-import axios from "axios";
+import { useState } from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
 const Lunch = () => {
-  const [lunchData, setLunchData] = useState([]);
-  useEffect(() => {
-    axios
-      .post(
-        "https://port-0-timetable-backend-kvmh2mlk183p67.sel4.cloudtype.app/viewtimetable",
-        { date: 20230511 },
-      )
-      .then((response) => {
-        setLunchData(response.data);
-      })
-      .catch((error) => {
-        if (axios.isCancel(error)) {
-          console.log("Request canceled", error.message);
-        } else {
-          console.log(error);
-        }
-      });
-  }, []);
-  return (
-    <div>
-      <div>하이</div>
-        <div>{lunchData.row.ORPLC_INFO}</div>
-        <div>{lunchData.row.CAL_INFO}</div>
-    </div>
-  );
+    const [lunchData, setLunchData] = useState();
+    useEffect(() => {
+        axios
+            .post('https://port-0-timetable-backend-kvmh2mlk183p67.sel4.cloudtype.app/lunch/mealinfo', { month: 202304 })
+            .then((response) => {
+                setLunchData(response.data);
+            })
+            .catch((error) => {
+                if (axios.isCancel(error)) {
+                    console.log('Request canceled', error.message);
+                } else {
+                    console.log(error);
+                }
+            });
+    }, []);
+    console.log(lunchData)
+    return (
+        <>
+            <div>하이</div>
+            <div>
+              {lunchData && lunchData.map((item) => (
+                <>
+                  <div>{item.date}</div>
+                  <div>{item.lunch}</div>
+                  <div>{item.calorie}</div>
+                </>
+              ))}
+            </div>
+        </>
+    );
 };
 
 export default Lunch;
