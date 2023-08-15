ae30c63a0d70fc3d8855b3ef6f5fa1a56c3aae6e
wbhaao <a31620504@gmail.com>
2023-08-15 18:30:56 +0900

    다됌

--- src/Pages/Lunch.jsx
+++ src/Pages/Lunch.jsx
@@ -7,26 +7,27 @@ import Footer from "../Components/Footer";
 import Meal from "../Components/Meal";
 
 const Lunch = () => {
-  const date = new Date();
+  const date = new Date(2023, 4, 17);
   const [lunchList, setLunchList] = useState([]);
-  const [lunchIndex, setLunchIndex] = useState(1);
+  const [rightIndex, setRightIndex] = useState(1);
+  const [leftIndex, setLeftIndex] = useState(1);
 
   const callLunch = (props) => {
     axios
       .get(
         "https://port-0-timetable-backend-kvmh2mlk183p67.sel4.cloudtype.app/lunch/mealinfo",
         {
-          params: { date:"2023", start:null, end:null }
+          params: { date:null, start:props.start ? format(props.start, "yyyyMMdd") : null, end:props.end ? format(props.end, "yyyyMMdd") : null }
         }, 
       )
       .then((response) => {
         if (!props.date) {
           console.log(response.data)
-          response.data.map((value, index) =>{
-            if (Number(value.date.month) == date.getMonth()+1 && Number(value.date.month) == date.getDay()){
-              setLunchIndex(index)
-            }
-          })
+          // response.data.map((value, index) =>{
+          //   if (Number(value.date.month) == date.getMonth()+1 && Number(value.date.month) == date.getDay()){
+          //     setLunchIndex(index)
+          //   }
+          // })
           return setLunchList(response.data);
         }
         if (!lunchList) {
@@ -36,6 +37,7 @@ const Lunch = () => {
           console.log("급식 데이터가 없습니다.");
           return;
         }
+        
       })
       .catch((error) => {
         console.log(error)
@@ -45,36 +47,13 @@ const Lunch = () => {
 
   useEffect(() => {
     callLunch({ start: subDays(date, 1), end: addDays(date, 1) });
+    console.log(lunchList)
   }, []);
 
   useEffect(() => {
-    if (!lunchList[lunchIndex + 1] && lunchList[lunchIndex]) {
-      const lunchDate = lunchList[lunchIndex].date;
-      callLunch({
-        date: addDays(
-          new Date(
-            lunchDate.year,
-            Number(lunchDate.month),
-            Number(lunchDate.day),
-          ),
-          1,
-        ),
-      });
-    } else if (lunchIndex === 0 && lunchList[0]) {
-      const lunchDate = lunchList[0].date;
-      callLunch({
-        date: subDays(
-          new Date(
-            lunchDate.year,
-            Number(lunchDate.month),
-            Number(lunchDate.day),
-          ),
-          1,
-        ),
-      });
-    }
+    callLunch({ start: subDays(date, rightIndex), end: addDays(date, leftIndex) });
     console.log(lunchList)
-  }, [lunchIndex]);
+  }, [rightIndex, leftIndex]);
   
   return (
     <>
@@ -82,20 +61,20 @@ const Lunch = () => {
       <div className="lunchBox-container">
         <FiChevronLeft
           size={70}
-          onClick={() => setLunchIndex(lunchIndex - 1)}
+          onClick={() => {setRightIndex(rightIndex+1);setLeftIndex(leftIndex-1)}}
         />
         <Meal
-          meal={lunchList[lunchIndex - 1] ? lunchList[lunchIndex - 1] : null}
-          onclick={() => setLunchIndex(lunchIndex - 1)}
+          meal={lunchList[0] ? lunchList[0] : null}
+          onclick={() => {setRightIndex(rightIndex+1);setLeftIndex(leftIndex-1)}}
         />
         <Meal
-          meal={lunchList[lunchIndex + 1] ? lunchList[lunchIndex + 1] : null}
-          onclick={() => setLunchIndex(lunchIndex + 1)}
+          meal={lunchList[2] ? lunchList[2] : null}
+          onclick={() => {setLeftIndex(leftIndex+1);setRightIndex(rightIndex-1);}}
         />
-        <Meal meal={lunchList[lunchIndex] ? lunchList[lunchIndex] : null} />
+        <Meal meal={lunchList[1] ? lunchList[1] : null} />
         <FiChevronRight
           size={70}
-          onClick={() => setLunchIndex(lunchIndex + 1)}
+          onClick={() => {setLeftIndex(leftIndex+1);setRightIndex(rightIndex-1);}}
         />
       </div>
       <Footer />
